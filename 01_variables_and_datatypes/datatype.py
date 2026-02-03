629e121333964c26e082b9a962601cc0787b280f
mansi_gajjar <mansigajjar1326@gmail.com>
2026-02-03 11:37:58 -0600

    Added variables and data types practice

---
+++ 01_variables_and_datatypes/datatype.py
@@ -0,0 +1,20 @@
+"""
+data_types.py
+Examples of basic data types in Python
+"""
+
+# Integer
+number = 10
+print("Number:", number, "| Type:", type(number))
+
+# Float
+price = 99.50
+print("Price:", price, "| Type:", type(price))
+
+# String
+course = "Python Programming"
+print("Course:", course, "| Type:", type(course))
+
+# Boolean
+is_python_easy = True
+print("Is Python Easy:", is_python_easy, "| Type:", type(is_python_easy))
---
+++ 01_variables_and_datatypes/variables.py
@@ -0,0 +1,20 @@
+"""
+variables.py
+Basic examples of variables in Python
+"""
+
+# Variable declaration
+name = "Mansi"
+age = 20
+height = 5.3
+is_student = True
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Updating a variable
+age = 21
+print("Updated Age:", age)
